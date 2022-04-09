f2bec7d91cea037b237490e589ab8ae7f2711238
shivaspk <shivaspk.siva@gmail.com>
2022-04-09 14:32:07 +0530

    updtes

--- todoapi/app.js
+++ todoapi/app.js
@@ -18,5 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/todos', todosRouter);
+expressOasGenerator.handleRequests(app, {});
 module.exports = app;
